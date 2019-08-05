918519bb017489954cd496e5a581b2431c0e8456
Jonas Leininger <jonas.leininger@gmail.com>
2019-08-05 14:15:11 +0200

    init

---
+++ hello.py
@@ -0,0 +1,63 @@
+import pybullet as p
+import time
+import pybullet_data
+
+def getJointNames(quadruped):
+    nJoints = p.getNumJoints(quadruped)
+    jointNameToId = {}
+
+    for i in range(nJoints):
+        jointInfo = p.getJointInfo(quadruped, i)
+        jointNameToId[jointInfo[1].decode('UTF-8')] = jointInfo[0]
+    return jointNameToId
+
+
+physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
+p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
+p.setGravity(0,0,-10)
+planeId = p.loadURDF("plane.urdf")
+cubeStartPos = [0,0,0]
+cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
+# boxId = p.loadURDF("r2d2.urdf",cubeStartPos, cubeStartOrientation)
+armId = p.loadURDF("cougarbot.urdf",cubeStartPos, cubeStartOrientation)
+jointNameToId = getJointNames(armId)
+
+kp = 0.1  # 0.012
+kd = 10.4  # .2
+maxForce = 120.5
+p.setJointMotorControl2(bodyIndex=armId,
+                        jointIndex=jointNameToId["hip"],
+                        controlMode=p.POSITION_CONTROL,
+                        targetPosition=-3.141,
+                        positionGain=kp,
+                        velocityGain=kd,
+                        force=maxForce)
+
+maxForceShoulder = 100.5
+p.setJointMotorControl2(bodyIndex=armId,
+                        jointIndex=jointNameToId["shoulder"],
+                        controlMode=p.POSITION_CONTROL,
+                        targetPosition=-0.5,
+                        positionGain=kp,
+                        velocityGain=kd,
+                        force=maxForceShoulder)
+
+maxForceElbow = 30
+p.setJointMotorControl2(bodyIndex=armId,
+                        jointIndex=jointNameToId["elbow"],
+                        controlMode=p.POSITION_CONTROL,
+                        targetPosition=0.5,
+                        positionGain=kp,
+                        velocityGain=kd,
+                        force=maxForceElbow)
+
+cubePos, cubeOrn = p.getBasePositionAndOrientation(armId)
+asf = p.getJointInfo(armId, jointNameToId["elbow"])
+print(asf)
+print('Position :', cubePos, ' ',cubeOrn)
+while True:
+    p.stepSimulation()
+    time.sleep(1./240.)
+cubePos, cubeOrn = p.getBasePositionAndOrientation(armId)
+print(cubePos,cubeOrn)
+p.disconnect()
