90a0bcfd44c057277ad1dc02f07890e64ff75775
Jonas Leininger <jonas.leininger@gmail.com>
2019-08-12 11:14:52 +0200

    refactor test script

--- test_env.py
+++ test_env.py
@@ -6,11 +6,16 @@ from robotEnv import RobotEnv
 def main():
     robotEnv = RobotEnv()
     print(RobotEnv.action_space)
-    for i in range(10000):
-        random_action = np.random.random(size=robotEnv.robot.n_joints - 1)
-        robotEnv.step(random_action)
-        obs = robotEnv.robot.get_observation()
-        if robotEnv.terminated == 1:
+    for i in range(100):
+        robotEnv.reset()
+        run_steps(robotEnv)
+
+def run_steps(robot_env):
+    while True:
+        random_action = np.random.random(size=robot_env.robot.n_joints - 1)
+        robot_env.step(random_action)
+        obs = robot_env.robot.get_observation()
+        if robot_env.terminated == 1:
             break
         p.stepSimulation()
 
